f21303334bb11146b42c7ab5cd519dceb7dcbed5
Jelena <radovanovic.jeca95@gmail.com>
2021-09-19 21:16:15 +0200

    completed

---
+++ src/App.tsx
@@ -0,0 +1,22 @@
+import React, { useState } from 'react';
+import './App.css';
+import Main from './components/Main/Main'
+import Header from './components/Header/Header'
+import { Route } from 'react-router-dom';
+import Work from './components/Work/Work';
+import { HashRouter as Router } from 'react-router-dom'
+
+function App() {
+
+  return (
+    <div className="App">
+        <Router basename={process.env.PUBLIC_URL}>
+          <Header />
+          <Route exact path="/" component={Main}></Route>
+          <Route path="/work/:id" component={Work}/>
+        </Router>
+    </div>
+  );
+}
+
+export default App;
